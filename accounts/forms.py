82848acefd3b0866323d8738fc04e3fbdb0b4d9b
sotthang <sotthang@gmail.com>
2023-04-27 14:49:19 +0900

    set accounts

---
+++ accounts/forms.py
@@ -0,0 +1,72 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm, PasswordChangeForm
+from django import forms
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = get_user_model()
+        fields = (
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'password1',
+            'password2',
+        )
+        widgets = {
+            'username': forms.TextInput(attrs={'class':'form-control',}),
+            'email': forms.TextInput(attrs={'class':'form-control',}),
+            'first_name': forms.TextInput(attrs={'class':'form-control',}),
+            'last_name': forms.TextInput(attrs={'class':'form-control',}),
+
+        }
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = get_user_model()
+        fields = (
+            'email', 
+            'first_name',
+            'last_name',
+        )
+        widgets = {
+            'email': forms.TextInput(attrs={'class':'form-control'}),
+            'first_name': forms.TextInput(attrs={'class':'form-control'}),
+            'last_name': forms.TextInput(attrs={'class':'form-control'}),
+        }
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(
+        label='아이디',
+        label_suffix='',
+        required=True,
+        widget=forms.TextInput(attrs={'class':'form-control'}),
+    )
+    password = forms.CharField(
+        label='비밀번호',
+        label_suffix='',
+        widget=forms.PasswordInput(attrs={'class':'form-control'}),
+    )
+
+
+class CustomPasswordChangeForm(PasswordChangeForm):
+    old_password = forms.CharField(
+        label='기존 비밀번호',
+        label_suffix='',
+        required=True,
+        widget=forms.PasswordInput(attrs={'class':'form-control'}),
+    )
+    new_password1 = forms.CharField(
+        label='새로운 비밀번호',
+        label_suffix='',
+        required=True,
+        widget=forms.PasswordInput(attrs={'class':'form-control'}),
+    )
+    new_password2 = forms.CharField(
+        label='새로운 비밀번호 확인',
+        label_suffix='',
+        required=True,
+        widget=forms.PasswordInput(attrs={'class':'form-control'}),
+    )
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm, CustomUserChangeForm, CustomPasswordChangeForm, LoginForm
 
 # Create your views here.
 
 def login(request):
     return render(request, 'accounts/login.html')
 
+
+def logout(request):
+    return render(request, 'accounts/login.html')
+
+
+def signup(request):
+    return render(request, 'accounts/signup.html')
+
+
+def delete(request):
+    return render(request, 'accounts/login.html')
+
+
+def update(request):
+    return render(request, 'accounts/update.html')
+
+
+def change_password(request):
+    return render(request, 'accounts/change_password.html')
 
