6a0ba280c4b76b9f8f53729faa479762c3a67cdd
Soozie <kam2301@naver.com>
2023-04-28 12:02:38 +0900

    complete articles/form.py

--- articles/forms.py
+++ articles/forms.py
@@ -1,15 +1,89 @@
 from django import forms
-from .models import Article, Comment
+from .models import Article, Comment, Review, ReviewComment, Emote
 
 
 class ArticleForm(forms.ModelForm):
+    CATEGORY_CHOICES = [
+        ('숙소','숙소'),
+        ('자연명소','자연명소'),
+        ('테마파크','테마파크'),
+        ('체험관광','체험관광'),
+        ('레저 & 액티비티','레저 & 액티비티'),
+        ('맛집','맛집'),
+        ('쇼핑','쇼핑'),
+
+    ]
+    category = forms.ChoiceField(
+        label = '카테고리',
+        widget = forms.Select(
+            attrs = {
+                'class': 'my-category form-control',
+                
+            }
+        ),
+        choices = CATEGORY_CHOICES,
+    )
     class Meta:
         model = Article
-        fields = ('title', 'content', 'image',)
+        exclude = ('user', 'emote_users')
+        labels = {
+            'title': '제목',
+            'content': '내용',
+            'image': '이미지',    
+
+
+        }
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control',}),
+            'category': forms.TextInput(attrs={'class':'form-control',}),
+            'content': forms.Textarea(attrs={'class':'form-control', 'rows':'5',}),
+
+        }
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('content',)
+        labels = {
+            'content': '댓글',
+        }
+        widgets = {
+            'content': forms.Textarea(attrs={'class':'form-control', 'rows':'2',})
+        }
+        
+        
+class ReviewForm(forms.ModelForm):
+    model = Review
+    exclude = ('user', 'emote_users')
+    labels = {
+            'title': '제목',
+            'content': '내용',
+            'image': '이미지',    
+            'score': '별점',
 
+
+        }
+    widgets = {
+        'title': forms.TextInput(attrs={'class':'form-control',}),
+        'content': forms.Textarea(attrs={'class':'form-control', 'rows':'5',}),
+        'score': forms.NumberInput(attrs={'class':'form-control',}),
+    }
+    
+
+class ReviewCommentForm(forms.ModelForm):
+    class Meta:
+        model = ReviewComment
+        fields = ('content',)
+        labels = {
+            'content': '댓글',
+        }
+        widgets = {
+            'content': forms.Textarea(attrs={'class':'form-control', 'rows':'2',})
+        }
+        
+class EmoteForm(forms.ModelForm):
+    class Meta:
+        model = Emote
+        fields = ('emotion',)
+        
--- articles/models.py
+++ articles/models.py
@@ -9,7 +9,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class Article(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
-    category = models.CharField(max_length=50)
+    category = models.CharField(max_length=50, default='체험관광')
     content = models.CharField(max_length=1000)
     image = ProcessedImageField(upload_to='', processors=[ResizeToFill(500, 250)], format='JPEG',options={'quality': 100})
     created_at = models.DateTimeField(auto_now_add=True)
